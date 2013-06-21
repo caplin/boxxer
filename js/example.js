74188d4cdd6b3ec2f3de5425a86a0ad25d0dd371
Bence <kormos.bence@gmail.com>
2013-06-21 09:00:22 +0100

    Adds caplin implementation example

---
+++ js/example.js
@@ -0,0 +1,29 @@
+
+var myChart = function() {
+
+
+
+};
+
+
+
+
+// When it is appended to the dom
+myChart.prototype.onOpen = function(nWidth, nHeight) {
+
+};
+
+myChart.prototype.onClose = function() {
+
+};
+
+myChart.prototype.onResize = function(nWidth, nHeight) {
+
+};
+
+
+
+
+boxxer.createBox({
+    component: new myChart()
+});
