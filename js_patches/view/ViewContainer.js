ad25c67ac2a1c5675fbcba372e3d6abadb822e6b
gillescochez <git@csurl.eu>
2013-06-29 12:45:48 +0100

    Removed viewContainer patch. Going to support interface as flag on build as useful interface even for non caplin components building.

--- js_patches/view/ViewContainer.js
+++
@@ -1,6 +0,0 @@
-boxxer.utils.patch("view.ViewContainer", function (ViewContainer) {
-
-    caplin.implement(ViewContainer, caplin.component.Component);
-
-    //TODO: implement Component interface properly
-});
